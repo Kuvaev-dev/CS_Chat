55b18a7e10dbb13701d1c1a743284e39b1710813
Somewhere <66011798+Kuvaev-dev@users.noreply.github.com>
2021-07-12 18:33:57 +0300

    Init

---
+++ Chat/ClientApp/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ClientApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // на сервере всё тоже самое
+            Console.OutputEncoding = Encoding.UTF8;
+            byte[] data = new byte[1024];
+            string input, stringData;       // ввод, считываемые данные
+            IPEndPoint ipep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9050);
+            Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            // создаём точку соединения и делаем проверку на ex
+            try
+            {
+                server.Connect(ipep);
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("!-> Не удалось подключиться.");
+                Console.WriteLine(e.ToString());
+                return;
+            }
+            int recv = server.Receive(data);
+            // задаём кодировку для считываемых данных
+            stringData = Encoding.UTF8.GetString(data, 0, recv);
+            Console.WriteLine(stringData);
+            while (true)
+            {
+                input = Console.ReadLine();
+                // задаём команду выхода
+                if (input == "--exit")
+                    break;
+
+                Console.WriteLine("-> You: " + input);
+                server.Send(Encoding.UTF8.GetBytes(input));
+                data = new byte[1024];
+                recv = server.Receive(data);
+                stringData = Encoding.UTF8.GetString(data, 0, recv);
+                byte[] utf8string = System.Text.Encoding.UTF8.GetBytes(stringData);
+                Console.WriteLine("-> Server: " + stringData);
+            }
+            Console.WriteLine("!-> Отключение от сервера...");
+            server.Shutdown(SocketShutdown.Both);
+            server.Close();
+            Console.WriteLine("!-> Соединение завершено!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Chat/ServerApp/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // задаём кодировку консоли по-умолчанию
+            Console.OutputEncoding = Encoding.UTF8;
+            // сюда помещаем Recieve()
+            int recv;
+            // задаём объём данных
+            byte[] data = new byte[1024];
+            // создаём конечную точку адреса (задаётся пользователем программы)
+            IPEndPoint ipep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8080);
+            // инициализируем сокет
+            Socket newsock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            // биндим сокет на получаемый адрес
+            newsock.Bind(ipep);
+            // ожидание
+            newsock.Listen(10);
+            Console.WriteLine("!-> Ожидание клиента...");
+            // подключаем клиента
+            Socket client = newsock.Accept();
+            IPEndPoint clientep = (IPEndPoint)client.RemoteEndPoint;
+            Console.WriteLine("!-> Connected with {0} at port {1}", clientep.Address, clientep.Port);
+            string welcome = "----- Добро пожаловать на сервер! -----";
+            // задаём кодировку приветственного сообщения
+            data = Encoding.UTF8.GetBytes(welcome);
+            // подсоединяем клиента
+            client.Send(data, data.Length, SocketFlags.None);
+            // строка клиента
+            string input;
+            // создаём бесконечный цикл
+            while (true)
+            {
+                // задаём объём данных
+                data = new byte[1024];
+                // создаём обработку данных
+                recv = client.Receive(data);
+                // делаем проверку на 0
+                if (recv == 0)
+                    break;
+                // вводим сообщение клиента
+                Console.WriteLine("-> Client: " + Encoding.UTF8.GetString(data, 0, recv));
+                input = Console.ReadLine();
+                // перемещаем курсор
+                Console.SetCursorPosition(0, Console.CursorTop - 1);
+                Console.WriteLine("-> You: " + input);
+                // получаем байтовый размер
+                client.Send(Encoding.UTF8.GetBytes(input));
+            }
+            // отсоединение
+            Console.WriteLine("!-> Disconnected from {0}", clientep.Address);
+            client.Close();
+            newsock.Close();
+            Console.ReadLine();
+        }
+    }
+}
